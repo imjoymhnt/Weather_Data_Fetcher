a8792e4a77a000d3034bc5c5e2c755382a267e3d
Mritunjoy Mahanta <mritunjoy.mahanta001@gmail.com>
2021-01-12 12:30:01 +0530

    dynamically getting diferent location weather report and also added the celcius and fehrenheit option

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const https = require("https");
-const { send } = require("process");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=bolpur&appid=8a823d0048da666a11786ed30025a9c4&units=metric";
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  const place = req.body.city;
+  const apiKey = "8a823d0048da666a11786ed30025a9c4";
+  let unit;
+  if (req.body.metric) {
+    unit = "metric";
+  } else if (req.body.imperial) {
+    unit = "imperial";
+  } else {
+    unit = "metric";
+  }
+  const sayUnit = unit === "imperial" ? "Fahrenheit" : "Celcius";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${apiKey}&units=${unit}`;
   https.get(url, (response) => {
     response.on("data", (data) => {
       const weatherData = JSON.parse(data);
@@ -15,9 +31,9 @@ app.get("/", (req, res) => {
       const icon = weatherData.weather[0].icon;
       const imgURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
 
-      res.write(`<p>The weather is ${desc}</p>`);
+      res.write(`<h1>The weather is ${desc}</h1>`);
       res.write(
-        `<h1>The temperature of bolpur is ${temp} degree celcius.</h1>`
+        `<h1>The temperature of ${place} is ${temp} degree ${sayUnit}.</h1>`
       );
       res.write(`<img src=${imgURL} >`);
       res.send();
